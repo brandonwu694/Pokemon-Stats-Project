660d35bbcba2e39d2a743a6e44e77b38e89d9d42
Brandon Wu <brwu1@uci.edu>
2026-04-10 12:17:56 -0700

    Finish error analysis of tuned xgboost model

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
+MODELS_DIR = BASE_DIR / "models"
